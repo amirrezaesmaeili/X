96f81cbd390656d53f151402f51ae2c204d581e2
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-28 23:14:55 -0400

    change  register direction path to class variable

--- account/views.py
+++ account/views.py
@@ -7,10 +7,11 @@ from django.contrib import messages
 
 class RegisterView(View):
     form_class = UserRegistrationForm
+    template_name = 'account/register.html'
 
     def get(self,request):
         form = self.form_class()
-        return render(request,'account/register.html',{'form':form})
+        return render(request,self.template_name,{'form':form})
        
     def post(self,request):
         form = self.form_class(request.POST)
@@ -19,5 +20,5 @@ class RegisterView(View):
             User.objects.create_user(cd['username'],cd['email'],cd['password'])
             messages.success(request,'you registered successfuly','success')
             return redirect('home:home')
-        return render(request,'account/register.html',{'form':form})
+        return render(request,self.template_name,{'form':form})
 
