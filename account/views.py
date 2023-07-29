cd9bc64db7f611eb42f0acf505ee139e37ff7741
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-28 21:59:44 -0400

    import User&Message in views.account

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 class RegisterView(View):
