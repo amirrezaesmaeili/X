efddfecd45ec293e0ab24a1c34b6feb7bc182294
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-29 22:35:12 -0400

    change name cd password to password1

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-class UserReg istrationForm(forms.Form):
+class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':' please Enter strong password'}))
