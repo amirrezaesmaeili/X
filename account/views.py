3848b0e7911abe1e0667ff879ca1d54b4fe45773
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-30 20:41:18 -0400

    import logout modUL

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from .forms import UserRegistrationForm,UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 class UserRegisterView(View):
