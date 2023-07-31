91a64cd626ca95cea033ddeca8b0c2d1749125f6
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-30 22:41:29 -0400

    add function get_user in authenticate class

--- account/authenticate.py
+++ account/authenticate.py
@@ -10,3 +10,10 @@ class EmailBackend:
             return None
         except User.DoesNotExist:
             return None
+    
+
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
