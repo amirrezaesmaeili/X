4ce71b0668a2adf68eb762983f6b1b6f72bbc730
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-30 22:00:56 -0400

    userLoginView clas inheritenth  LoginRequiredMixin

--- account/views.py
+++ account/views.py
@@ -58,7 +58,7 @@ class UserLoginView(View):
         return render(request,self.template_name,{'form':form})
             
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin,View):
     def get(self,request):
         logout(request)
         messages.success(request,'you logged out successfuly', 'success')
