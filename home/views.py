3b188b976d51058a0aac97bee678590dc578e497
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-31 22:08:08 -0400

    add query all for show posts& context in def get

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
 # Create your views here.
 
 
 class HomeView(View):
     def get(self,request):
-        return render(request,'home/index.html')
+        posts = Post.objects.all()
+        return render(request,'home/index.html',{'posts':posts})
     
     
