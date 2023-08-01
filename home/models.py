f12439e4ee7cd7399a8e2c3d7248b342eb892fe5
amirrezaesmaeili <esmaili69315@gmail.com>
2023-07-31 21:35:45 -0400

    add auto_now_add&auto_now for automat date in post

--- home/models.py
+++ home/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     body = models.TextField()
     slug = models.SlugField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.slug} - {self.updated}'
