e5d51d0cbe9864172677cd395327107544353333
amirrezaesmaeili <esmaili69315@gmail.com>
2023-08-01 20:57:43 -0400

    imp reverse in models.py for access django to urls

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
